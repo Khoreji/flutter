7671bb1cd64da7c8d9389bcc7bffcdfae08c2e60
khoreji <gopalakhoria@gmail.com>
2021-05-04 07:40:33 +0530

    hey

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: Scaffold(
+        bottomNavigationBar: CurvedNavigationBar(
+          backgroundColor: Colors.blue,
+        items: [
+          Icon(Icons.add, size: 30,),
+          Icon(Icons.web, size: 30),
+          Icon(Icons.person, size: 30),
+        ],
+        ),
+      ),
+    );
+  }
+}
